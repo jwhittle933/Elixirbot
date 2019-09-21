c0c57210b3fc15a969d860c8ed611b47f7f350be
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-09-20 20:30:05 -0400

    start testing

--- test/elixirbot_test.exs
+++ test/elixirbot_test.exs
@@ -1,8 +1,21 @@
 defmodule ElixirbotTest do
   use ExUnit.Case
+  use Plug.Test
+  alias Elixirbot.Router
   doctest Elixirbot
+
+  @opts Router.init([])
 
   test "greets the world" do
     assert Elixirbot.hello() == :world
+  end
+
+  test "Response" do
+    conn = conn(:post, "/webhook", "") |> Router.call(@opts)
+
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "Oh yeah"
   end
 end
