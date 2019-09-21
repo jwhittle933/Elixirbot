cb0f056c87a9a26473ceb2f812a5eaed89776ee3
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-09-20 20:04:26 -0400

    new deps

--- mix.exs
+++ mix.exs
@@ -23,6 +23,8 @@ defmodule Elixirbot.MixProject do
   defp deps do
     [
       {:slack, "~> 0.6.0"},
+      {:cowboy, "~>2.6"},
+      {:plug, "~>1.8"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
