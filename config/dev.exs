43ab8f0f0e403336aa701da23fdfe15fb854d996
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-09-26 08:40:08 -0400

    ok

---
+++ config/dev.exs
@@ -0,0 +1,11 @@
+import Config
+
+config :elixirbot,
+  slack_client_id: 564541565235.767680482628,
+  slack_client_secret: "a0a2230e0f1c7a96f2cc01e0b37df0c6",
+  slack_signing_secret: "f343e5892171437e86622961f54df000",
+  slack_verification_token: "mfrw3xqQCKS3qTaviTh9wTCC",
+  slack_webhook_url: "https://hooks.slack.com/services/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"
+
+config :elixirbot,
+  port: 4000
