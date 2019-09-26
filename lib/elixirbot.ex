7b5586ebf66cd26393acc2152a333e37ea4660d0
Jonathan Whittle <jonathan.whittle@eltoro.com>
2019-09-26 08:59:55 -0400

    Correct issues (#8)
    * fixes
    
    * remove
    
    * fixes

--- lib/elixirbot.ex
+++ lib/elixirbot.ex
@@ -2,13 +2,4 @@ defmodule Elixirbot do
   @moduledoc """
   Documentation for Elixirbot.
   """
-  use GenServer
-
-  def start_link(child_spec) do
-    GenServer.start_link(__MODULE__, [child_spec])
-  end
-
-  def init([child_spec]) do
-    {:ok, child_spec}
-  end
 end
