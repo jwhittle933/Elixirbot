e1499db689353e9cba6514df1e4c7cd10ac50915
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-09-26 08:42:45 -0400

    fix

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Elixirbot.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :slack],
+      extra_applications: [:logger, :slack, :eex],
       mod: {Elixirbot.Application, []}
     ]
   end
